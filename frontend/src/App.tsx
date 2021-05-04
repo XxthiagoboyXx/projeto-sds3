963b441fb2d870bff22df3cb6570347d1ee2fc91
Thiago Santos <thiagoboy34@gmail.com>
2021-05-03 21:05:02 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,18 @@
+import DataTable from "components/DataTable";
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+
 function App() {
   return (
-    <div>
-      <h1 className="text-primary">Fala cachorra!</h1>
-    </div>
+    <> {/*<> e </> é conhecido como fragment e server para burlar a checagem do retorno da função só ter um elemento */}
+      <NavBar />
+      <div className="container"> {/*essa classe container é padrão da bootstrap e serve para alinhar o elemento*/}
+        <h1 className="text-primary">Fala cachorra!</h1>
+        <DataTable />
+      </div>
+
+      <Footer />
+    </>
   );
 }
 
---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,14 @@
+function Footer() {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/XxthiagoboyXx" target="_blank" rel="noreferrer">Thiago Santos</a></p>
+                <p className="text-light">Contato: <a href="https://instagram.com/thiago.log" target="_blank" rel="noreferrer">@thiago.log </a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong></small></p>
+            </div>
+        </footer>
+    );
+
+}
+
+export default Footer;
