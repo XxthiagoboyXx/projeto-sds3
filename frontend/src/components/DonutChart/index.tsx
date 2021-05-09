bd624d7cabf2b924b4b8f408725227d9319e1ea7
Thiago Santos <thiagoboy34@gmail.com>
2021-05-09 19:27:19 -0300

    DonutChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts'
 import { SaleSum } from 'types/sale';
 import { BASE_URL } from 'utils/requests';
@@ -10,17 +11,21 @@ type ChartData = {
 
 const DonutChart = () => {
 
-    let chartData: ChartData = { labels: [], series: [] }; //let pq é uma variavel que muda e nao uma constante
+    const [chartData, setChartData] = useState<ChartData>({ labels: [], series: [] });
 
-    axios.get(`${BASE_URL}/sales/amount-by-seller`)
-        .then((response) => {
-            const data = response.data as SaleSum[];
-            const myLabels = data.map(x => x.sellerName); //para cada x do SaleUm eu vou levar no x.sellerName
-            const mySeries = data.map(x => x.sum);
+    useEffect(() => {
+        axios.get(`${BASE_URL}/sales/amount-by-seller`)
+            .then((response) => {
+                const data = response.data as SaleSum[];
+                const myLabels = data.map(x => x.sellerName); //para cada x do SaleUm eu vou levar no x.sellerName
+                const mySeries = data.map(x => x.sum);
 
-            chartData = { labels: myLabels, series: mySeries };
-            console.log(chartData);
-        });
+                setChartData({ labels: myLabels, series: mySeries });
+            });
+    }, []);
+
+
+
 
     //const mockData = {
     //  series: [477138, 499928, 444867, 220426, 473088],
