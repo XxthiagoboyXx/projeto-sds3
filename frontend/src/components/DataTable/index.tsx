19ab858e2ca7800f7813b5bdef34d86262b82693
Thiago Santos <thiagoboy34@gmail.com>
2021-05-09 22:19:40 -0300

    Pagination

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -1,4 +1,5 @@
 import axios from "axios";
+import Pagination from "components/Pagination";
 import { useEffect, useState } from "react";
 import { SalePage } from "types/sale";
 import { formatLocalDate } from "utils/format";
@@ -6,6 +7,7 @@ import { BASE_URL } from "utils/requests";
 
 const DataTable = () => { //a mesma coisa da outra notação, no caso, DataTable é uma variável que contem o retorno de uma função
 
+    const [activePage, setActivePage] = useState(0); //valor inicial = 0
     const [page, setPage] = useState<SalePage>({
         first: true,
         last: true,
@@ -15,39 +17,46 @@ const DataTable = () => { //a mesma coisa da outra notação, no caso, DataTable
     });
 
     useEffect(() => {
-        axios.get(`${BASE_URL}/sales?page=0&size=20&sort=date,desc`)
+        axios.get(`${BASE_URL}/sales?page=${activePage}&size=20&sort=date,desc`)
             .then(response => {
                 setPage(response.data);
             })
-    }, []);
+    }, [activePage]);
+
+    const changePage = (index: number) => {
+        setActivePage(index);
+    }
 
     return (
-        <div className="table-responsive"> {/*Classes definidas pelo bootstrap*/}
-            <table className="table table-striped table-sm">
-                <thead>
-                    <tr>
-                        <th>Data</th>
-                        <th>Vendedor</th>
-                        <th>Cliente</th>
-                        <th>Negócios fechados</th>
-                        <th>Valor</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {page.content?.map(item => ( //se o page.content estiver definido vou chamar o o .map e para cada item vou retornar uma variável
-                        <tr key={item.id}> {/* o react pede para que cada item da coleção se use um id unico */}
-                            <td>{formatLocalDate(item.date, "dd/MM/yyyy")}</td>
-                            <td>{item.seller.name}</td>
-                            <td>{item.visited}</td>
-                            <td>{item.deals}</td>
-                            <td>{item.amount.toFixed(2)}</td> {/*imprimir duas casas decimais*/}
+        <>
+            <Pagination page={page} onPageChange={changePage} />
+            <div className="table-responsive"> {/*Classes definidas pelo bootstrap*/}
+                <table className="table table-striped table-sm">
+                    <thead>
+                        <tr>
+                            <th>Data</th>
+                            <th>Vendedor</th>
+                            <th>Cliente</th>
+                            <th>Negócios fechados</th>
+                            <th>Valor</th>
                         </tr>
-                    ))}
+                    </thead>
+                    <tbody>
+                        {page.content?.map(item => ( //se o page.content estiver definido vou chamar o o .map e para cada item vou retornar uma variável
+                            <tr key={item.id}> {/* o react pede para que cada item da coleção se use um id unico */}
+                                <td>{formatLocalDate(item.date, "dd/MM/yyyy")}</td>
+                                <td>{item.seller.name}</td>
+                                <td>{item.visited}</td>
+                                <td>{item.deals}</td>
+                                <td>{item.amount.toFixed(2)}</td> {/*imprimir duas casas decimais*/}
+                            </tr>
+                        ))}
 
 
-                </tbody>
-            </table>
-        </div>
+                    </tbody>
+                </table>
+            </div>
+        </>
     );
 }
 
